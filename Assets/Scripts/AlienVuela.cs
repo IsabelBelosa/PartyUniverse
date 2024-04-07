d39deb761c4ad920bc5f712b5a9c458de622b466
Mari <116094046+jesmalean@users.noreply.github.com>
2024-04-07 22:33:57 +0200

    Portada/Menu

--- Assets/Scripts/AlienVuela.cs
+++ Assets/Scripts/AlienVuela.cs
@@ -1,4 +1,7 @@
+using System.ComponentModel;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class AlienVuela : MonoBehaviour
 {
@@ -25,73 +28,94 @@ public class AlienVuela : MonoBehaviour
     public float scaleRate;
     private float scaleTimer;
 
+    public Camera camara;
+    bool animacionActiva = false;
+    public AudioClip mareo;
+    public AudioClip cohete;
+
+
     void Start()
     {
         // Obtener el componente Animator del objeto
         animator = GetComponent<Animator>();
+        
     }
 
     void Update()
     {
-        // Cambiar a la primera animación cuando se presiona la tecla "1"
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             CambiarAnimacion("rig|SpaceGuyPose_ZeroGravity_1");
-                    if(isAnimated)
+            animacionActiva = true;
+            transform.position = new Vector3(-7.103f, 6.060875f, -30.312f);
+            camara.transform.position = new Vector3(-7.343821f, 6.620454f, -27.85598f);
+            GetComponent<AudioSource>().clip = mareo; 
+            GetComponent<AudioSource>().Play();
+        }
+
+        if (animacionActiva)
         {
-            if(isRotating)
+            // Realiza las acciones de animación
+            if(isAnimated)
             {
-                transform.Rotate(rotationAngle * rotationSpeed * Time.deltaTime);
-            }
-
-            if(isFloating)
-            {
-                floatTimer += Time.deltaTime;
-                Vector3 moveDir = new Vector3(0.0f, floatSpeed, 0.0f );
-                transform.Translate(moveDir);
-
-                if (goingUp && floatTimer >= floatRate)
+                if(isRotating)
                 {
-                    goingUp = false;
-                    floatTimer = 0;
-                    floatSpeed = -floatSpeed;
+                    transform.Rotate(rotationAngle * rotationSpeed * Time.deltaTime);
                 }
 
-                else if(!goingUp && floatTimer >= floatRate)
+                if(isFloating)
                 {
-                    goingUp = true;
-                    floatTimer = 0;
-                    floatSpeed = +floatSpeed;
-                }
-            }
+                    floatTimer += Time.deltaTime;
+                    Vector3 moveDir = new Vector3(0.0f, floatSpeed, 0.0f);
+                    transform.Translate(moveDir);
 
-            if(isScaling)
-            {
-                scaleTimer += Time.deltaTime;
-
-                if (scalingUp)
-                {
-                    transform.localScale = Vector3.Lerp(transform.localScale, endScale, scaleSpeed * Time.deltaTime);
+                    if (goingUp && floatTimer >= floatRate)
+                    {
+                        goingUp = false;
+                        floatTimer = 0;
+                        floatSpeed = -floatSpeed;
+                    }
+                    else if (!goingUp && floatTimer >= floatRate)
+                    {
+                        goingUp = true;
+                        floatTimer = 0;
+                        floatSpeed = +floatSpeed;
+                    }
                 }
-                else if (!scalingUp)
-                {
-                    transform.localScale = Vector3.Lerp(transform.localScale, startScale, scaleSpeed * Time.deltaTime);
-                }
 
-                if(scaleTimer >= scaleRate)
+                if(isScaling)
                 {
-                    if (scalingUp) { scalingUp = false; }
-                    else if (!scalingUp) { scalingUp = true; }
-                    scaleTimer = 0;
+                    scaleTimer += Time.deltaTime;
+
+                    if (scalingUp)
+                    {
+                        transform.localScale = Vector3.Lerp(transform.localScale, endScale, scaleSpeed * Time.deltaTime);
+                    }
+                    else if (!scalingUp)
+                    {
+                        transform.localScale = Vector3.Lerp(transform.localScale, startScale, scaleSpeed * Time.deltaTime);
+                    }
+
+                    if(scaleTimer >= scaleRate)
+                    {
+                        if (scalingUp) { scalingUp = false; }
+                        else if (!scalingUp) { scalingUp = true; }
+                        scaleTimer = 0;
+                    }
                 }
             }
         }
-        }
-
         // Cambiar a la segunda animación cuando se presiona la tecla "2"
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
+            animacionActiva = false;
             CambiarAnimacion("rig|SpaceGuyPose_Standing");
+            transform.position = new Vector3(-9.543f, 7.37f, -33.427f);
+            transform.rotation = new Quaternion(0f,0f,0f,0f);
+            GetComponent<AudioSource>().clip = cohete; 
+            GetComponent<AudioSource>().Play();
+            camara.transform.position = new Vector3(-9.465719f, 8.143775f, -28.78481f);
+            camara.transform.rotation = new Quaternion(18.3588505f,180.281967f,-18.3588505f,0f);
         }
     }
 
